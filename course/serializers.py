56e3e420a41593723898105e274a23a61a39814d
ArtemSermyagin <artem.sermyagin@yahoo.com>
2024-05-08 11:22:09 +0400

    домашка готова 24.2

--- course/serializers.py
+++ course/serializers.py
@@ -1,16 +1,38 @@
 from rest_framework import serializers
 
 from course.models import Course, Lesson
-
-
-# Serializers define the API representation.
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
+from users.models import Payment
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(read_only=True, many=True)
+    count_lessons = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'name',
+            'description',
+            'preview',
+            'count_lessons',
+            'lessons'
+        )
+
+    def get_count_lessons(self, obj: Course):
+        return obj.lessons.all().count()
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+
+
+
