e577cea4b9c87760956a3f0e0ff900ddc03eb17d
somebody <somebody@mail.ru>
2024-05-25 11:26:08 +0300

    send mail before updater course

--- course/tasks.py
+++ course/tasks.py
@@ -1,14 +1,19 @@
 from django.core.mail import send_mail
 
 from DRF.celery import app
+from DRF.settings import EMAIL_HOST_USER
+from course.models import Course
+from users.models import Subscription
 
 
 @app.task
 def send_mail_update_course(course_id):
+    course = Course.objects.get(pk=course_id)
+    subscriptions = Subscription.objects.filter(course=course)
+    recipient_list = [subscription.user.email for subscription in subscriptions]
     send_mail(
         'Course Updated',
-        f'The course "1',
-        'from@example.com',
-        ['to@example.com'],
-        fail_silently=False,
+        f'The course {course.name} has been updated.',
+        EMAIL_HOST_USER,
+        recipient_list,
     )
